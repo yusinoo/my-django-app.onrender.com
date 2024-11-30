e69dd876de6e5be48bfc37d21a5d9b7487f471ae
xavier rigales <xavierrigales1@gmail.com>
2024-11-29 20:12:21 -0700

    Add logic to prepopulate superuser

--- manage.py
+++ manage.py
@@ -2,21 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.contrib.auth.models import User
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
     try:
-        # Import and create superuser
         from django.core.management import execute_from_command_line
-        
-        # Prepopulate superuser
-        import django
-        django.setup()  # Initialize Django
-        if not User.objects.filter(username="your_username").exists():
-            User.objects.create_superuser("your_username", "your_email@example.com", "your_password")
-            print("Superuser created successfully!")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
