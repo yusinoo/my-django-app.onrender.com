f8180af1d61aec83f2509af2955773ac05d72c9d
xavier rigales <xavierrigales1@gmail.com>
2024-11-29 19:00:31 -0700

    Add STATIC_ROOT to settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
