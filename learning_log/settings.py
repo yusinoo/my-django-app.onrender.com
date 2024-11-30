1c8492f0e45978757b0c66cf1c6d90e1d8aa50d0
xavier rigales <xavierrigales1@gmail.com>
2024-11-29 21:36:48 -0700

    Fix logout functionality and update base.html

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_u+8srpt98*9-9mp7_dx3dl9^gu$__#(er&vjq_hica)69vs-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','my-django-app.onrender.com', 'my-django-app-onrender-com.onrender.com']
 
