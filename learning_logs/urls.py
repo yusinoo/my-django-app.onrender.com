b4b4a1fc67018479e3ec837329370aea0b0dc80e
xavier rigales <xavierrigales1@gmail.com>
2024-11-29 20:50:59 -0700

    Set up user authentication and restricted access

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
